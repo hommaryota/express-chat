966c665bc00dce34176a9b15d769edac1780a9d0
hommaryota <ryo4111016@gmail.com>
2024-04-20 23:03:28 +0900

    htmlを表示できるように変更

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const io = require('socket.io')(server);
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send("Hello World!")
+  res.sendFile(__dirname + '/index.html')
 })
 server.listen(PORT, () => {
   console.log('listening on *:3000');
